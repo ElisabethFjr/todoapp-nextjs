9b05089cfbf10e4b22c5200076a8e7031ba84bc3
Elisabeth FAUJOUR <efaujour@gmail.com>
2024-02-07 14:37:42 +0100

    :sparkles: API route POST GET List

--- prisma/migrations/20240207133500_init/migration.sql
+++ prisma/migrations/20240207133500_init/migration.sql
@@ -1,21 +1,10 @@
 -- CreateTable
-CREATE TABLE "User" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "password" TEXT NOT NULL,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "List" (
     "id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "color" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
-    "userId" TEXT NOT NULL,
 
     CONSTRAINT "List_pkey" PRIMARY KEY ("id")
 );
@@ -31,12 +20,6 @@ CREATE TABLE "Task" (
 
     CONSTRAINT "Task_pkey" PRIMARY KEY ("id")
 );
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-
--- AddForeignKey
-ALTER TABLE "List" ADD CONSTRAINT "List_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Task" ADD CONSTRAINT "Task_listId_fkey" FOREIGN KEY ("listId") REFERENCES "List"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/app/api/list/route.ts
+++ src/app/api/list/route.ts
@@ -1,10 +1,52 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
+import { Task } from "@/@types";
 
-// GET Lists with Tasks
+// GET /api/list All Lists with Tasks
+export async function GET() {
+  try {
+    const lists = await prisma.list.findMany({
+      include: {
+        tasks: true, // Include tasks associated with the list
+      },
+    });
+    return NextResponse.json(lists);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Oops, an error occurs, please try again later." },
+      { status: 500 }
+    );
+  }
+}
 
-// POST List
+// POST /api/list Create List
+export async function POST(req: NextRequest) {
+  const body = await req.json();
+  try {
+    const { title, color, tasks } = body;
+    const newList = await prisma.list.create({
+      data: {
+        title: title,
+        color: color,
+        tasks: {
+          createMany: {
+            data: tasks.map((task: Task) => ({
+              text: task.text,
+              is_completed: task.is_completed || false,
+            })),
+          },
+        },
+      },
+      include: { tasks: true },
+    });
 
-// UPDATE List
-
-// DELETE List
+    return NextResponse.json(newList);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Oops, an error occurs, please try again later." },
+      { status: 500 }
+    );
+  }
+}
