464e3aa319f20be58b138024b79bbe0491d3bd3c
Elisabeth FAUJOUR <efaujour@gmail.com>
2024-02-07 13:26:38 +0100

    :sparkles: Setup API routes

---
+++ src/app/api/list/[id]/route.ts
@@ -0,0 +1,8 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+// GET List id
+
+// UPDATE List id
+
+// DELETE List id
---
+++ src/app/api/list/route.ts
@@ -0,0 +1,10 @@
+import { NextRequest, NextResponse } from "next/server";
+import prisma from "@/lib/prisma";
+
+// GET Lists with Tasks
+
+// POST List
+
+// UPDATE List
+
+// DELETE List
