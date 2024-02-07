197115f72b9da26d1e0828078ef8e2959e03c5c0
Elisabeth FAUJOUR <efaujour@gmail.com>
2024-02-07 20:15:28 +0100

    :bug: Comment code Error Type

--- src/app/api/list/route.ts
+++ src/app/api/list/route.ts
@@ -21,41 +21,41 @@ export async function GET() {
 }
 
 // POST /api/list Create List with Tasks
-export async function POST(req: NextRequest) {
-  const body = await req.json();
-  try {
-    const { title, color, tasks } = body;
+// export async function POST(req: NextRequest) {
+//   const body = await req.json();
+//   try {
+//     const { title, color, tasks } = body;
 
-    // Check if color and tasks are provided in the request body
-    if (!title || !color || !tasks) {
-      return NextResponse.json(
-        { message: "Required fields." },
-        { status: 400 }
-      );
-    }
+//     // Check if color and tasks are provided in the request body
+//     if (!title || !color || !tasks) {
+//       return NextResponse.json(
+//         { message: "Required fields." },
+//         { status: 400 }
+//       );
+//     }
 
-    const newList = await prisma.list.create({
-      data: {
-        title: title,
-        color: color,
-        tasks: {
-          createMany: {
-            data: tasks.map((task: Task) => ({
-              text: task.text,
-              is_completed: task.is_completed || false,
-            })),
-          },
-        },
-      },
-      include: { tasks: true },
-    });
+//     const newList = await prisma.list.create({
+//       data: {
+//         title: title,
+//         color: color,
+//         tasks: {
+//           createMany: {
+//             data: tasks.map((task: Task) => ({
+//               text: task.text,
+//               is_completed: task.is_completed || false,
+//             })),
+//           },
+//         },
+//       },
+//       include: { tasks: true },
+//     });
 
-    return NextResponse.json(newList);
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      { message: "Oops, an error occurs, please try again later." },
-      { status: 500 }
-    );
-  }
-}
+//     return NextResponse.json(newList);
+//   } catch (error) {
+//     console.error(error);
+//     return NextResponse.json(
+//       { message: "Oops, an error occurs, please try again later." },
+//       { status: 500 }
+//     );
+//   }
+// }
