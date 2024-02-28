d1ae5d37a86de8a893842d580822f4f67bcac81c
Elisabeth FAUJOUR <efaujour@gmail.com>
2024-02-28 18:26:32 +0100

    :sparkles: Setup DragNDrop on Lists

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,6 @@ async function Home({ searchParams }: HomeProps) {
 
   // Fetch all Lists
   const lists = await getAllLists();
-  console.log("Listes", lists);
 
   return (
     <>
--- src/components/lists/Lists.tsx
+++ src/components/lists/Lists.tsx
@@ -1,5 +1,14 @@
-import NoList from "./noList/NoList";
+"use client";
+
+import {
+  DragDropContext,
+  Droppable,
+  Draggable,
+  DropResult,
+} from "react-beautiful-dnd";
+import { useState } from "react";
 import List from "./list/List";
+import NoList from "./noList/NoList";
 import styles from "./Lists.module.scss";
 import { List as ListType } from "@/@types";
 
@@ -8,21 +17,71 @@ interface ListsProps {
 }
 
 function Lists({ lists }: ListsProps) {
+  const [listsData, setListsData] = useState<ListType[]>(lists);
+  console.log("ListsData", listsData);
+
+  const handleOnDragEnd = (result: DropResult) => {
+    console.log("HandleDragEnd");
+    const { source, destination } = result;
+
+    console.log("Source", source);
+    console.log("Destination", destination);
+
+    if (
+      !destination ||
+      (destination.index === source.index &&
+        destination.droppableId === source.droppableId)
+    ) {
+      return;
+    }
+
+    const updatedLists = Array.from(listsData);
+    const [reorderedItem] = updatedLists.splice(result.source.index, 1);
+    if (result.destination) {
+      updatedLists.splice(result.destination.index, 0, reorderedItem);
+    }
+    setListsData(updatedLists);
+  };
+
   return (
-    <div className={styles.container}>
-      {/* Lists or Nolist */}
-      {lists.length > 0 ? (
-        <div className={styles.lists}>
-          {lists.map((list: ListType) => (
-            <List key={list.id} list={list} />
-          ))}
-        </div>
-      ) : (
-        <div className={styles.nolist}>
-          <NoList />
-        </div>
-      )}
-    </div>
+    <DragDropContext onDragEnd={handleOnDragEnd}>
+      <div className={styles.container}>
+        {listsData.length > 0 ? (
+          <Droppable droppableId="lists">
+            {(provided) => (
+              <ul
+                className={styles.lists}
+                ref={provided.innerRef}
+                {...provided.droppableProps}
+              >
+                {listsData.map((list: ListType, index: number) => (
+                  <Draggable
+                    key={list.id}
+                    draggableId={list.id.toString()}
+                    index={index}
+                  >
+                    {(provided) => (
+                      <li
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                      >
+                        <List list={list} />
+                      </li>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </ul>
+            )}
+          </Droppable>
+        ) : (
+          <div className={styles.nolist}>
+            <NoList />
+          </div>
+        )}
+      </div>
+    </DragDropContext>
   );
 }
 
